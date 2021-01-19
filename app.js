1690f6a5d17157b066451f723bba6639379c6a90
Greg Swan <greg.c.swan@gmail.com>
2021-01-18 19:45:23 -0600

    updated static served code

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/people', (req, res) => {
     { id: 2, firstName: 'Somebody', lastName: 'Else' }
   ])
 })
+app.get("/",(req,res)=>{
+    app.use(express.static(__dirname+"/client/build/static"));            //required for css and js
+    app.use(express.static('./client/build/', express.static('static')));  //required for images and fonts
+    res.sendFile(__dirname + "/client/build/index.html");
+})
 
 app.listen(port, () => {
   console.log(`Logging ANOTHER_ENV_VAR: ${process.env.ANOTHER_ENV_VAR}`)
